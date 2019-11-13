88eeee89f4eb4a88b7797532bc61971374f436da
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2019-11-13 12:09:12 -0500

    Add onos-topo API docs (#53)

--- build/bin/compile-protos.sh
+++ build/bin/compile-protos.sh
@@ -2,6 +2,6 @@
 
 proto_imports=".:${GOPATH}/src/github.com/gogo/protobuf/protobuf:${GOPATH}/src/github.com/gogo/protobuf:${GOPATH}/src"
 
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/admin,plugins=grpc:. api/admin/*.proto
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/device,plugins=grpc:. api/device/*.proto
-protoc -I=$proto_imports --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/diags,plugins=grpc:. api/diags/*.proto
+protoc -I=$proto_imports --doc_out=docs/api  --doc_opt=markdown,admin.md --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/admin,plugins=grpc:. api/admin/*.proto
+protoc -I=$proto_imports --doc_out=docs/api  --doc_opt=markdown,device.md --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/device,plugins=grpc:. api/device/*.proto
+protoc -I=$proto_imports --doc_out=docs/api  --doc_opt=markdown,diags.md --gogofaster_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,import_path=topo/diags,plugins=grpc:. api/diags/*.proto
