395985753e2e853a3e9f4fec659e73e1100d2c68
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2020-03-04 20:10:47 -0800

    Add E2AP protocol type (#95)

--- api/admin/admin.pb.go
+++ api/admin/admin.pb.go
@@ -22,7 +22,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 func init() { proto.RegisterFile("api/admin/admin.proto", fileDescriptor_d6b467461202c036) }
 
