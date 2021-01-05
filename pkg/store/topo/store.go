3154595f389c242c7c9184299956fa4f4ffee679
Sean Condon <sean@opennetworking.org>
2021-01-05 11:37:06 +0000

    Watch event should be UPDATE by default

--- pkg/store/topo/store.go
+++ pkg/store/topo/store.go
@@ -238,10 +238,10 @@ func (s *atomixStore) Watch(ctx context.Context, ch chan<- topoapi.Event, opts .
 					eventType = topoapi.EventType_NONE
 				case _map.EventInserted:
 					eventType = topoapi.EventType_ADDED
-				case _map.EventUpdated:
-					eventType = topoapi.EventType_UPDATED
 				case _map.EventRemoved:
 					eventType = topoapi.EventType_REMOVED
+				default:
+					eventType = topoapi.EventType_UPDATED
 				}
 				ch <- topoapi.Event{
 					Type:   eventType,
