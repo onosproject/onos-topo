e86c48e3fad476ec4c535fafafd085d28536d177
Ray Milkey <ray@opennetworking.org>
2021-04-30 19:44:31 -0700

    Allow specifying a registry through helmit (#221)

--- test/topo/suite.go
+++ test/topo/suite.go
@@ -16,6 +16,7 @@ package topo
 
 import (
 	"github.com/onosproject/helmit/pkg/helm"
+	"github.com/onosproject/helmit/pkg/input"
 	"github.com/onosproject/helmit/pkg/test"
 	"github.com/onosproject/onos-test/pkg/onostest"
 )
@@ -32,11 +33,13 @@ type TestSuite struct {
 const onosTopoComponentName = "onos-topo"
 
 // SetupTestSuite sets up the onos-topo test suite
-func (s *TestSuite) SetupTestSuite() error {
+func (s *TestSuite) SetupTestSuite(c *input.Context) error {
 
+	registry := c.GetArg("registry").String("")
 	err := helm.Chart(onosTopoComponentName, onostest.OnosChartRepo).
 		Release(onosTopoComponentName).
 		Set("image.tag", "latest").
+		Set("global.image.registry", registry).
 		Install(true)
 	if err != nil {
 		return err
