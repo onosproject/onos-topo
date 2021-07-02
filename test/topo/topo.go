33f0989f47010a6a67c1bac6c6c815452ebebaff
Ivan Eroshkin <erijj@mail.ru>
2021-07-02 18:35:38 +0200

    Updated with regard to latest conversation with Adib

--- test/topo/topo.go
+++ test/topo/topo.go
@@ -6,19 +6,12 @@ package topo
 
 import (
 	"context"
-	"github.com/atomix/atomix-go-client/pkg/atomix/test"
 	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 	utils "github.com/onosproject/onos-topo/test/utils"
 	"testing"
 
 	"gotest.tools/assert"
-
-	"github.com/atomix/atomix-go-client/pkg/atomix/test/rsm"
-)
-
-const (
-	numRequestedE2Nodes = 50
 )
 
 //var (
@@ -30,17 +23,11 @@ const (
 var log = logging.GetLogger("topo")
 
 func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
-	test := test.NewTest(
-		rsm.NewProtocol(),
-		test.WithReplicas(1),
-		test.WithPartitions(1))
-	assert.NilError(t, test.Start())
-	defer test.Stop()
 
-	conn := utils.CreateServerConnection(t, test)
+	conn, err := utils.CreateConnection()
 	client := topoapi.NewTopoClient(conn)
 
-	_, err := client.Create(context.Background(), &topoapi.CreateRequest{
+	_, err = client.Create(context.Background(), &topoapi.CreateRequest{
 		Object: &topoapi.Object{
 			ID:   "1",
 			Type: topoapi.Object_ENTITY,
@@ -93,96 +80,3 @@ func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
 	//	return len(res.Objects) == 1 && res.Objects[0].ID == "2"
 	//})
 }
-
-//func (s *TestSuite) TestAddRemoveDevice1(t *testing.T) {
-//
-//	//ToDo - how to define it properly?? What is the correct way?
-//	//st, err := utils.NewClient()
-//	//assert.NilError(t, err, "Error creating client")
-//
-//	//topo := topoapi.Object{}
-//	//err = st.Create(context.Background(), &topo)
-//	//assert.NilError(t, err, "Error creating topo")
-//
-//	st := utils.Rnib{}
-//	//st, err := utils.NewStore()
-//	//assert.NilError(t, err, "Updating device failed"
-//
-//	e2NodeID := topoapi.ID("13b4f7")
-//
-//	// ToDo - should I take it from somewhere else?
-//	serviceModels := make(map[string]*topoapi.ServiceModelInfo)
-//	serviceModels["kpmv2"] = &topoapi.ServiceModelInfo{
-//		OID:  "1.3.6.1.4.1.53148.1.2.2.2",
-//		Name: "KPMv2",
-//	}
-//
-//	// create or update node entities
-//	err := st.CreateOrUpdateNode(context.Background(), e2NodeID, serviceModels)
-//	assert.NilError(t, err, "Updating device failed")
-//
-//	// checking whether it is actually stored
-//	out, err := st.GetRelation(context.Background(), e2NodeID)
-//	assert.NilError(t, err, "Updating device failed")
-//	assert.Equal(t, out, e2NodeID)
-//
-//	// m stands for channel manager
-//	// removing device from the store
-//	err = st.DeleteRelation(context.Background(), out)
-//	assert.NilError(t, err, "Removing device failed")
-//
-//	// checking whether it was deleted
-//	res, _ := st.GetRelation(context.Background(), e2NodeID)
-//	//assert.NilError(t, err, "Updating device failed")
-//	assert.Equal(t, res, topoapi.ID(""))
-//}
-
-//func (s *TestSuite) TestMultiE2Nodes(t *testing.T) {
-//
-//	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-//	defer cancel()
-//	topoSdkClient, err := utils.NewTopoClient()
-//	assert.NilError(t, err)
-//	topoEventChan := make(chan topoapi.Event)
-//	err = topoSdkClient.WatchE2Connections(ctx, topoEventChan)
-//	assert.NilError(t, err)
-//
-//	nodeClient := utils.GetRansimNodeClient(t, sim)
-//	assert.Assert(t, nodeClient != nil)
-//
-//	defaultNumNodes := utils.GetNumNodes(t, nodeClient)
-//
-//	for i := 0; i < numRequestedE2Nodes; i++ {
-//		enbID := i + initialEnbID
-//		createNodeRequest := &modelapi.CreateNodeRequest{
-//			Node: &ransimtypes.Node{
-//				GnbID:         ransimtypes.GnbID(enbID),
-//				ServiceModels: serviceModels,
-//				Controllers:   controllers,
-//				CellNCGIs:     []ransimtypes.NCGI{},
-//			},
-//		}
-//		e2node, err := nodeClient.CreateNode(ctx, createNodeRequest)
-//		assert.NilError(t, err)
-//		assert.Assert(t, e2node != nil)
-//	}
-//	numNodes := utils.GetNumNodes(t, nodeClient)
-//	assert.Equal(t, numRequestedE2Nodes+defaultNumNodes, numNodes)
-//
-//	utils.CountTopoAddedOrNoneEvent(topoEventChan, numNodes)
-//
-//	e2nodes := utils.GetNodes(t, nodeClient)
-//	for _, e2node := range e2nodes {
-//		_, err = nodeClient.DeleteNode(ctx, &modelapi.DeleteNodeRequest{
-//			GnbID: e2node.GnbID,
-//		})
-//		assert.NoError(t, err)
-//	}
-//
-//	utils.CountTopoRemovedEvent(topoEventChan, numNodes)
-//
-//	numNodes = utils.GetNumNodes(t, nodeClient)
-//	assert.Equal(t, 0, numNodes)
-//	err = sim.Uninstall()
-//	assert.NilError(t, err)
-//}
--- test/utils/topo.go
+++ test/utils/topo.go
@@ -5,184 +5,14 @@
 package utils
 
 import (
-	"context"
 	"crypto/tls"
-	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	"github.com/onosproject/onos-lib-go/pkg/certs"
-	"github.com/onosproject/onos-lib-go/pkg/errors"
-	//"github.com/onosproject/onos-lib-go/pkg/logging"
-	"github.com/onosproject/onos-lib-go/pkg/southbound"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/status"
-	"io"
-	"time"
 )
 
 //var log = logging.GetLogger("utils")
-
-// Client is a topo SDK client
-type Client interface {
-	// Create creates a topo object
-	Create(ctx context.Context, object *topoapi.Object) error
-
-	// Update updates a topo object
-	Update(ctx context.Context, object *topoapi.Object) error
-
-	// Get gets a topo object with a given ID
-	Get(ctx context.Context, id topoapi.ID) (*topoapi.Object, error)
-
-	// Watch provides a simple facility for the application to watch for changes in the topology
-	Watch(ctx context.Context, ch chan<- topoapi.Event, opts ...WatchOption) error
-
-	// List of topo objects
-	List(ctx context.Context, opts ...ListOption) ([]topoapi.Object, error)
-}
-
-// NewClient creates a new topo client
-func NewClient(opts ...Option) (Client, error) {
-	clientOptions := Options{}
-
-	for _, opt := range opts {
-		opt.apply(&clientOptions)
-	}
-
-	if clientOptions.Service.Host == "" || clientOptions.Service.Port == 0 {
-		clientOptions.Service.Host = DefaultServiceHost
-		clientOptions.Service.Port = DefaultServicePort
-	}
-
-	dialOpts := []grpc.DialOption{
-		grpc.WithStreamInterceptor(southbound.RetryingStreamClientInterceptor(100 * time.Millisecond)),
-		grpc.WithUnaryInterceptor(southbound.RetryingUnaryClientInterceptor()),
-	}
-	if clientOptions.Service.Insecure {
-		dialOpts = append(dialOpts, grpc.WithInsecure())
-	} else {
-		tlsConfig, err := GetClientCredentials()
-		if err != nil {
-			log.Warn(err)
-			return nil, err
-		}
-
-		dialOpts = append(dialOpts, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
-	}
-
-	//ToDo - how to deal with it? Where to get a manager?
-	conns := NewManager()
-	conn, err := conns.Connect(clientOptions.Service.GetAddress(), dialOpts...)
-	if err != nil {
-		return nil, err
-	}
-
-	cl := topoapi.NewTopoClient(conn)
-
-	return &topo{
-		client: cl,
-	}, nil
-}
-
-// topo is the topo client
-type topo struct {
-	client topoapi.TopoClient
-}
-
-// Create creates a topo object
-func (t *topo) Create(ctx context.Context, object *topoapi.Object) error {
-	response, err := t.client.Create(ctx, &topoapi.CreateRequest{
-		Object: object,
-	})
-	if err != nil {
-		return errors.FromGRPC(err)
-	}
-	*object = *response.Object
-	return nil
-}
-
-// Update updates a given topo object
-func (t *topo) Update(ctx context.Context, object *topoapi.Object) error {
-	response, err := t.client.Update(ctx, &topoapi.UpdateRequest{
-		Object: object,
-	})
-	if err != nil {
-		return errors.FromGRPC(err)
-	}
-
-	*object = *response.Object
-	return nil
-}
-
-// List lists all of topo objects
-func (t *topo) List(ctx context.Context, opts ...ListOption) ([]topoapi.Object, error) {
-	options := ListOptions{}
-
-	for _, opt := range opts {
-		opt.apply(&options)
-	}
-
-	response, err := t.client.List(ctx, &topoapi.ListRequest{
-		Filters: options.GetFilters(),
-	})
-	if err != nil {
-		return nil, errors.FromGRPC(err)
-	}
-
-	return response.GetObjects(), nil
-}
-
-// Get get a topo object based on a given ID
-func (t *topo) Get(ctx context.Context, id topoapi.ID) (*topoapi.Object, error) {
-	response, err := t.client.Get(ctx, &topoapi.GetRequest{
-		ID: id,
-	})
-	if err != nil {
-		return nil, errors.FromGRPC(err)
-	}
-	return response.GetObject(), nil
-}
-
-// Watch watches topology events
-func (t *topo) Watch(ctx context.Context, ch chan<- topoapi.Event, opts ...WatchOption) error {
-
-	options := WatchOptions{}
-	for _, opt := range opts {
-		opt.apply(&options)
-	}
-
-	req := topoapi.WatchRequest{
-		Filters: options.GetFilters(),
-	}
-	stream, err := t.client.Watch(ctx, &req)
-	if err != nil {
-		defer close(ch)
-		return errors.FromGRPC(err)
-	}
-
-	go func() {
-		defer close(ch)
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF || err == context.Canceled {
-				break
-			}
-
-			if err != nil {
-				stat, ok := status.FromError(err)
-				if ok {
-					err = errors.FromStatus(stat)
-					if errors.IsCanceled(err) || errors.IsTimeout(err) {
-						break
-					}
-				}
-				log.Error("An error occurred in receiving topology changes", err)
-			} else {
-				ch <- resp.Event
-			}
-		}
-	}()
-	return nil
-
-}
 
 func GetClientCredentials() (*tls.Config, error) {
 	cert, err := tls.X509KeyPair([]byte(certs.DefaultClientCrt), []byte(certs.DefaultClientKey))
@@ -193,4 +23,22 @@ func GetClientCredentials() (*tls.Config, error) {
 		Certificates:       []tls.Certificate{cert},
 		InsecureSkipVerify: true,
 	}, nil
+}
+
+func CreateConnection() (*grpc.ClientConn, error) {
+	tlsConfig, err := GetClientCredentials()
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)),
+	}
+
+	conn, err := grpc.Dial("onos-topo:5150", opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
