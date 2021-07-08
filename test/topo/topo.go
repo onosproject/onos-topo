1a1aba3158e5103237e6bbc50c9d4df2fd1c83c6
Thomas Vachuska <tom@opennetworking.org>
2021-07-08 14:41:03 -0700

    Adding integration test for scale and various topo queries

--- test/topo/topo.go
+++ test/topo/topo.go
@@ -16,6 +16,8 @@ package topo
 
 import (
 	"context"
+	"fmt"
+	"github.com/gogo/protobuf/types"
 	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	utils "github.com/onosproject/onos-topo/test/utils"
 	"testing"
@@ -23,9 +25,41 @@ import (
 	"gotest.tools/assert"
 )
 
+func createEntity(client topoapi.TopoClient, id string, kindID string, aspectList []*types.Any) error {
+	aspects := map[string]*types.Any{}
+	for _, aspect := range aspectList {
+		aspects[aspect.TypeUrl] = aspect
+	}
+	_, err := client.Create(context.Background(), &topoapi.CreateRequest{
+		Object: &topoapi.Object{
+			ID:      topoapi.ID(id),
+			Type:    topoapi.Object_ENTITY,
+			Aspects: aspects,
+			Obj:     &topoapi.Object_Entity{Entity: &topoapi.Entity{KindID: topoapi.ID(kindID)}},
+		},
+	})
+	return err
+}
+
+func createRelation(client topoapi.TopoClient, src string, tgt string, kindID string) error {
+	_, err := client.Create(context.Background(), &topoapi.CreateRequest{
+		Object: &topoapi.Object{
+			ID:   topoapi.ID(src + tgt + kindID),
+			Type: topoapi.Object_RELATION,
+			Obj: &topoapi.Object_Relation{
+				Relation: &topoapi.Relation{
+					SrcEntityID: topoapi.ID(src),
+					TgtEntityID: topoapi.ID(tgt),
+					KindID:      topoapi.ID(kindID),
+				},
+			},
+		},
+	})
+	return err
+}
+
 // TestAddRemoveDevice adds devices to the storage, lists and checks that they are in database and removes devices from the storage
 func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
-
 	t.Logf("Creating connection")
 	conn, err := utils.CreateConnection()
 	assert.NilError(t, err)
@@ -33,20 +67,16 @@ func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
 	client := topoapi.NewTopoClient(conn)
 
 	t.Logf("Adding first device to the topo store")
-	_, err = client.Create(context.Background(), &topoapi.CreateRequest{
-		Object: &topoapi.Object{
-			ID:   "1",
-			Type: topoapi.Object_ENTITY,
-		},
+	err = createEntity(client, "1", "testKind", []*types.Any{
+		{TypeUrl: "onos.topo.Location", Value: []byte(`{"lat": 123, "lng": 321}`)},
+		{TypeUrl: "foo", Value: []byte(`{"s": "barfoo", "n": 314, "b": true}`)},
 	})
 	assert.NilError(t, err)
 
 	t.Logf("Adding second device to the topo store")
-	_, err = client.Create(context.Background(), &topoapi.CreateRequest{
-		Object: &topoapi.Object{
-			ID:   "2",
-			Type: topoapi.Object_ENTITY,
-		},
+	err = createEntity(client, "2", "testKind", []*types.Any{
+		{TypeUrl: "onos.topo.Location", Value: []byte(`{"lat": 111, "lng": 222}`)},
+		{TypeUrl: "foo", Value: []byte(`{"s": "foobar", "n": 628, "b": true}`)},
 	})
 	assert.NilError(t, err)
 
@@ -66,6 +96,8 @@ func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
 
 	t.Logf("Updating first device")
 	obj := gres.Object
+	obj.Aspects["onos.topo.Location"] = &types.Any{TypeUrl: "onos.topo.Location", Value: []byte(`{"lat": 123, "lng": 321}`)}
+
 	ures, err := client.Update(context.Background(), &topoapi.UpdateRequest{
 		Object: obj,
 	})
@@ -82,4 +114,70 @@ func (s *TestSuite) TestAddRemoveDevice(t *testing.T) {
 	res, err = client.List(context.Background(), &topoapi.ListRequest{})
 	assert.NilError(t, err)
 	assert.Equal(t, len(res.Objects) == 1 && res.Objects[0].ID == "2", true)
+}
+
+func (s *TestSuite) TestScale(t *testing.T) {
+	t.Logf("Creating connection")
+	conn, err := utils.CreateConnection()
+	assert.NilError(t, err)
+	t.Logf("Creating Topo Client")
+	client := topoapi.NewTopoClient(conn)
+
+	t.Logf("Creating 100 nodes, with 6 cells each Nd 6 node-cell relations")
+	for n := 0; n < 100; n++ {
+		err = createEntity(client, fmt.Sprintf("node%d", n+1), "e2node", []*types.Any{{TypeUrl: "onos.topo.Location", Value: []byte(`{"lat": 123.0, "lng": 321.0}`)}})
+		assert.NilError(t, err)
+
+		for c := 0; c < 6; c++ {
+			err = createEntity(client, fmt.Sprintf("cell%d%d", n+1, c+1), "e2cell", []*types.Any{{TypeUrl: "onos.topo.Location", Value: []byte(`{"lat": 123.0, "lng": 321.0}`)}})
+			assert.NilError(t, err)
+			err = createRelation(client, fmt.Sprintf("node%d", n+1), fmt.Sprintf("cell%d%d", n+1, c+1), "contains")
+			assert.NilError(t, err)
+		}
+	}
+
+	// Filter e2nodes; there should be 100
+	t.Logf("Getting all 'e2nodes'")
+	res, err := client.List(context.Background(), &topoapi.ListRequest{Filters: &topoapi.Filters{
+		KindFilter: &topoapi.Filter{Filter: &topoapi.Filter_Equal_{Equal_: &topoapi.EqualFilter{Value: "e2node"}}},
+	}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Objects), 100)
+
+	// Filter e2cells; there should be 600
+	t.Logf("Getting all 'e2cells'")
+	res, err = client.List(context.Background(), &topoapi.ListRequest{Filters: &topoapi.Filters{
+		KindFilter: &topoapi.Filter{Filter: &topoapi.Filter_Equal_{Equal_: &topoapi.EqualFilter{Value: "e2cell"}}},
+	}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Objects), 600)
+
+	// Filter contains relations; there should be 600
+	t.Logf("Getting all 'contains' relations'")
+	res, err = client.List(context.Background(), &topoapi.ListRequest{Filters: &topoapi.Filters{
+		KindFilter: &topoapi.Filter{Filter: &topoapi.Filter_Equal_{Equal_: &topoapi.EqualFilter{Value: "contains"}}},
+	}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Objects), 600)
+
+	// Filter e2cells of an e2node; there should be 6
+	t.Logf("Getting all cells of a node")
+	res, err = client.List(context.Background(), &topoapi.ListRequest{Filters: &topoapi.Filters{
+		RelationFilter: &topoapi.RelationFilter{
+			SrcId:        "node10",
+			RelationKind: "contains",
+			TargetKind:   "e2cell",
+		},
+	}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Objects), 6)
+
+	// Filter e2nodes, e2cells, contains, but only entities; there should be 700
+	t.Logf("Getting all nodes and cells")
+	res, err = client.List(context.Background(), &topoapi.ListRequest{Filters: &topoapi.Filters{
+		KindFilter: &topoapi.Filter{Filter: &topoapi.Filter_In{In: &topoapi.InFilter{Values: []string{"e2node", "e2cell", "contains"}}}},
+		ObjectTypes: []topoapi.Object_Type{topoapi.Object_ENTITY},
+	}})
+	assert.NilError(t, err)
+	assert.Equal(t, len(res.Objects), 700)
 }
