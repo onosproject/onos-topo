c9bf7d177606e474ca480c1681c002cc34e9e6c4
eesha-deepak <54864240+eesha-deepak@users.noreply.github.com>
2022-08-01 16:25:29 -0700

    added pipelines to the tool and created a new template (#303)
    * added pipelines to the tool and created a new template
    
    * new template
    
    Co-authored-by: Ubuntu <eesha@eesha.sdfabric-ci1.hudson>

--- pkg/tools/topo-generator/generator/writer.go
+++ pkg/tools/topo-generator/generator/writer.go
@@ -13,12 +13,7 @@ import (
 )
 
 const (
-	networkTemplatePath    = "/onos-topo/pkg/tools/topo-generator/generator/templates/network.yaml"
-	switchTemplatePath     = "/onos-topo/pkg/tools/topo-generator/generator/templates/switch.yaml"
-	portTemplatePath       = "/onos-topo/pkg/tools/topo-generator/generator/templates/port.yaml"
-	linkTemplatePath       = "/onos-topo/pkg/tools/topo-generator/generator/templates/link.yaml"
-	originatesTemplatePath = "/onos-topo/pkg/tools/topo-generator/generator/templates/originates.yaml"
-	terminatesTemplatePath = "/onos-topo/pkg/tools/topo-generator/generator/templates/terminates.yaml"
+	templatePath = "/onos-topo/pkg/tools/topo-generator/generator/templates/template.yaml"
 )
 
 var log = logging.GetLogger()
@@ -28,93 +23,20 @@ func WriteFile(result parser.NetworkLayer, filename string) error {
 	// writing the entity-kind-relationship file
 	pwd, _ := os.Getwd()
 	s := strings.Split(pwd, "/onos-topo/")
-	networkPath := s[0] + networkTemplatePath
-	switchPath := s[0] + switchTemplatePath
-	portPath := s[0] + portTemplatePath
-	linkPath := s[0] + linkTemplatePath
-	originatesPath := s[0] + originatesTemplatePath
-	terminatesPath := s[0] + terminatesTemplatePath
+	path := s[0] + templatePath
 
 	file, _ := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	for _, n := range result.Networks {
-		// network
-		t, err := template.ParseFiles(networkPath)
-		if err != nil {
-			log.Error(err.Error())
-			return err
-		}
-		err = t.Execute(file, n)
-		if err != nil {
-			log.Error(err.Error())
-			return err
-		}
-
-		// switches
-		for _, s := range n.Switches {
-			t, err := template.ParseFiles(switchPath)
-			if err != nil {
-				log.Error(err.Error())
-				return err
-			}
-			err = t.Execute(file, s)
-			if err != nil {
-				log.Error(err.Error())
-				return err
-			}
-
-			// ports
-			for _, p := range s.Ports {
-				t, err := template.ParseFiles(portPath)
-				if err != nil {
-					log.Error(err.Error())
-					return err
-				}
-				err = t.Execute(file, p)
-				if err != nil {
-					log.Error(err.Error())
-					return err
-				}
-			}
-		}
-
-		// links
-		for _, l := range n.Links {
-			// handles whether the link is unidirectional or bidirectional
-			t, err := template.ParseFiles(linkPath)
-			if err != nil {
-				log.Error(err.Error())
-				return err
-			}
-			err = t.Execute(file, l)
-			if err != nil {
-				log.Error(err.Error())
-				return err
-			}
-
-			t1, err1 := template.ParseFiles(originatesPath)
-			if err1 != nil {
-				log.Error(err1.Error())
-				return err1
-			}
-			err1 = t1.Execute(file, l.OriginatesRelation)
-			if err1 != nil {
-				log.Error(err1.Error())
-				return err1
-			}
-
-			t2, err2 := template.ParseFiles(terminatesPath)
-			if err2 != nil {
-				log.Error(err2.Error())
-				return err2
-			}
-			err2 = t2.Execute(file, l.TerminatesRelation)
-			if err2 != nil {
-				log.Error(err2.Error())
-				return err2
-			}
-		}
+	t, err := template.ParseFiles(path)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
 	}
+	err = t.Execute(file, result)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	}
+
 	defer file.Close()
 	return nil
 }
