01e9ff769d570a23f29b24a7b5bdb7b3ea13b6d1
Thomas Vachuska <tom.vachuska@intel.com>
2022-12-21 19:15:51 -0800

    Bumped onos-lib-go dependency

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/onosproject/helmit v0.6.19
 	github.com/onosproject/onos-api/go v0.10.7
-	github.com/onosproject/onos-lib-go v0.9.5
+	github.com/onosproject/onos-lib-go v0.10.3
 	github.com/onosproject/onos-test v0.6.6
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -8,21 +8,19 @@ package manager
 import (
 	"github.com/atomix/go-sdk/pkg/client"
 	"github.com/atomix/go-sdk/pkg/primitive"
+	"github.com/onosproject/onos-lib-go/pkg/cli"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 	"github.com/onosproject/onos-lib-go/pkg/northbound"
 	service "github.com/onosproject/onos-topo/pkg/northbound"
 	"github.com/onosproject/onos-topo/pkg/store"
 )
 
-var log = logging.GetLogger()
+var log = logging.GetLogger("manager")
 
 // Config is a manager configuration
 type Config struct {
-	CAPath       string
-	KeyPath      string
-	CertPath     string
-	GRPCPort     int
 	AtomixClient primitive.Client
+	ServiceFlags *cli.ServiceEndpointFlags
 }
 
 // NewManager creates a new manager
@@ -35,19 +33,13 @@ func NewManager(config Config) *Manager {
 
 // Manager single point of entry for the topology system.
 type Manager struct {
+	cli.Daemon
 	Config Config
-}
-
-// Run starts a synchronizer based on the devices and the northbound services.
-func (m *Manager) Run() {
-	log.Info("Starting Manager")
-	if err := m.Start(); err != nil {
-		log.Fatal("Unable to run Manager", err)
-	}
 }
 
 // Start starts the manager
 func (m *Manager) Start() error {
+	log.Info("Starting Manager")
 	err := m.startNorthboundServer()
 	if err != nil {
 		return err
@@ -55,16 +47,14 @@ func (m *Manager) Start() error {
 	return nil
 }
 
+// Stop stops the channels and manager related objects
+func (m *Manager) Stop() {
+	log.Info("Stopping Manager")
+}
+
 // startNorthboundServer starts the northbound gRPC server
 func (m *Manager) startNorthboundServer() error {
-	s := northbound.NewServer(northbound.NewServerCfg(
-		m.Config.CAPath,
-		m.Config.KeyPath,
-		m.Config.CertPath,
-		int16(m.Config.GRPCPort),
-		true,
-		northbound.SecurityConfig{}))
-
+	s := northbound.NewServer(cli.ServerConfigFromFlags(m.Config.ServiceFlags, northbound.SecurityConfig{}))
 	if m.Config.AtomixClient == nil {
 		m.Config.AtomixClient = client.NewClient()
 	}
@@ -88,9 +78,4 @@ func (m *Manager) startNorthboundServer() error {
 		}
 	}()
 	return <-doneCh
-}
-
-// Close kills the channels and manager related objects
-func (m *Manager) Close() {
-	log.Info("Closing Manager")
 }
